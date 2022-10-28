a38d743144d28cfde04d4af904f7b8a44cc7113a
Johannes <84457538+p-jeff@users.noreply.github.com>
2022-10-28 19:27:28 +0200

    addes Stuff

--- src/routes/selectionWheel.svelte
+++ src/routes/selectionWheel.svelte
@@ -1,10 +1,23 @@
 <script>
+	import anime from 'animejs';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		let selectorAnimation = anime({
+			targets: '.selected'
+		});
+	});
+
 	let content = [
 		{ id: 1, name: 'Entry1' },
 		{ id: 2, name: 'Entry2' },
-		{ id: 3, name: 'Entry3' }
+		{ id: 3, name: 'Entry3' },
+		{ id: 4, name: 'Entry4' },
+		{ id: 5, name: 'Entry5' },
+		{ id: 6, name: 'Entry6' },
+		{ id: 7, name: 'Entry7' }
 	];
-	let selected = { id: 4, name: 'Entry4' };
+	let selected = { id: 8, name: 'Entry8' };
 
 	function changeSelection(direction) {
 		if (direction === 'up') {
@@ -35,19 +48,28 @@
 <svelte:window on:mousewheel={handleScroll} />
 
 <div>
-	{#each content as entry (entry.id)}
-		<h2>{entry.name}</h2>
+	{#each content as entry, i}
+		<h2 style:margin-left="{i * 30}px" style:font-size="{i * 5}px">
+			{entry.name}
+		</h2>
 	{/each}
 
-	<h2 class="selected">{selected.name}</h2>
+	<h2
+		class="selected"
+		style:margin-left="{content.length * 30}px"
+		style:font-size="{content.length * 5}px"
+	>
+		{selected.name}
+	</h2>
 </div>
 
 <style>
 	div {
 		background-color: aqua;
 		overflow: scroll;
-		width: 20%;
+		width: 100%;
 	}
+
 	.selected {
 		color: #ff0000;
 	}
