b5a2b27edbac14adafd2e0f547b04b6cbc0cde4a
Johannes <84457538+p-jeff@users.noreply.github.com>
2022-10-29 16:40:30 +0200

    Extended Config for commonJS

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { join } from 'path';
+import { readFileSync } from 'fs';
+import { cwd } from 'process';
+
+const pkg = JSON.parse(readFileSync(join(cwd(), 'package.json')));
+
 
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	ssr: {
+		noExternal: Object.keys(pkg.dependencies || {})
+	}
 };
 
 export default config;
